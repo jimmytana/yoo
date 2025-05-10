60062882ebb3df56574d34f432777fc7be481f1d
jimmytana <kubwimanajimmy96@gmail.com>
2025-05-10 15:03:34 +0200

    formate

--- exercises.py
+++ exercises.py
@@ -1,8 +1,8 @@
-a=[]
-n=int(input("how many student you want to find their average :"))
-for i in range(n):
-    p=int(input("please enter maks to find the aveerage :"))
-    a.append(p)
-print(a)
-avg=sum(a)/len(a)
-print("the average of entered marks is :",avg)
+def text_formater(text:str,suffix=True,preffix=True,capitalize=False):
+    text=text.capitalize()
+    print("the formatted text is as follows :",f"{suffix}{text}{preffix}")
+text=input("please enter a text to formate: ")
+suffix=input("enter the suffix :")
+preffix=input("please enter your prefed preffix :")
+
+a=text_formater(text,suffix,preffix,capitalize=True)
