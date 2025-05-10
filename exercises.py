50abc840b66f3685bd9f24a09798975b746382a0
jimmytana <Kubwimanajimmy96@gmail.com>
2025-05-10 15:47:27 +0200

    string cases done

--- exercises.py
+++ exercises.py
@@ -10,3 +10,17 @@ def pascal_to_camel(text:str):
 text=input("please enter your  pascal text format :")
 a=pascal_to_camel(text)
 print(f"the converted string is as follows : {a}")
+another case 
+
+
+def calmel_to_snake(calmel):
+    snake=" "
+    for char in calmel:
+        if char. isupper():
+            snake+="_"+char.lower()
+        else :
+            snake+=char
+    return snake.lstrip('_')
+calmel=input("please enter the text to convert in snake case :")
+a=calmel_to_snake(calmel)
+print (f"the converted string is :{a}")
